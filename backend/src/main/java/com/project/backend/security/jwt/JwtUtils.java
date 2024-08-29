9e8edad02399c489ad34602f20e29b1bf7b5c9e8
ruke79 <ruke79@naver.com>
2024-08-30 01:48:37 +0900

    jwt refresh token 추가

--- backend/src/main/java/com/project/backend/security/jwt/JwtUtils.java
+++ backend/src/main/java/com/project/backend/security/jwt/JwtUtils.java
@@ -9,13 +9,17 @@ import javax.crypto.SecretKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.WebUtils;
 
+import com.project.backend.model.User;
 import com.project.backend.security.service.UserDetailsImpl;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 
 
@@ -29,6 +33,34 @@ public class JwtUtils {
     @Value("${spring.app.jwtExpirationMs}")
     private int jwtExpirationMs;
 
+    @Value("${spring.app.jwtCookieName}")
+  private String jwtCookie;
+  
+  @Value("${spring.app.jwtRefreshCookieName}")
+  private String jwtRefreshCookie;
+
+  public ResponseCookie generateJwtCookie(UserDetailsImpl userDetails) {
+    String jwt = generateTokenFromUsername(userDetails);
+    return generateCookie(jwtCookie, jwt, "/api");
+  }
+
+  public ResponseCookie generateJwtCookie(User user) {
+    String jwt = generateTokenFromUsername(user);
+    return generateCookie(jwtCookie, jwt, "/api");
+  }
+
+  public ResponseCookie generateRefreshJwtCookie(String refreshToken) {
+    return generateCookie(jwtRefreshCookie, refreshToken, "/api/auth/refreshtoken");
+  }
+
+  public String getJwtFromCookies(HttpServletRequest request) {
+    return getCookieValueByName(request, jwtCookie);
+  }
+
+  public String getJwtRefreshFromCookies(HttpServletRequest request) {
+    return getCookieValueByName(request, jwtRefreshCookie);
+  }
+
     public String getJwtFromHeader(HttpServletRequest request) {
         String bearerToken = request.getHeader("Authorization");
         logger.debug("Authorization Header: {}", bearerToken);
@@ -37,6 +69,16 @@ public class JwtUtils {
         }
         return null;
     }
+
+    public ResponseCookie getCleanJwtCookie() {
+        ResponseCookie cookie = ResponseCookie.from(jwtCookie, null).path("/api").build();
+        return cookie;
+      }
+
+      public ResponseCookie getCleanJwtRefreshCookie() {
+        ResponseCookie cookie = ResponseCookie.from(jwtRefreshCookie, null).path("/api/auth/refreshtoken").build();
+        return cookie;
+      }
 
     public String generateTokenFromUsername(UserDetailsImpl userDetails) {
         //String username = userDetails.getUsername();
@@ -48,6 +90,20 @@ public class JwtUtils {
                 .subject(email)
                 .claim("roles", roles)
                 .claim("is2faEnabled", userDetails.is2faEnabled())
+                .issuedAt(new Date())
+                .expiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(key())
+                .compact();
+    }
+
+    public String generateTokenFromUsername(User user) {
+        //String username = userDetails.getUsername();
+        String email = user.getEmail();
+        String role = user.getRole().getRoleName().name();
+        return Jwts.builder()
+                .subject(email)
+                .claim("role", role)
+                .claim("is2faEnabled", user.isTwoFactorEnabled())
                 .issuedAt(new Date())
                 .expiration(new Date((new Date()).getTime() + jwtExpirationMs))
                 .signWith(key())
@@ -82,4 +138,18 @@ public class JwtUtils {
         }
         return false;
     }
+
+    private ResponseCookie generateCookie(String name, String value, String path) {
+        ResponseCookie cookie = ResponseCookie.from(name, value).path(path).maxAge(24 * 60 * 60).httpOnly(true).build();
+        return cookie;
+    }
+
+    private String getCookieValueByName(HttpServletRequest request, String name) {
+    Cookie cookie = WebUtils.getCookie(request, name);
+    if (cookie != null) {
+      return cookie.getValue();
+    } else {
+      return null;
+    }
+  }
 }
--- backend/src/main/java/com/project/backend/security/response/LoginResponse.java
+++ backend/src/main/java/com/project/backend/security/response/LoginResponse.java
@@ -1,5 +1,6 @@
 package com.project.backend.security.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,15 +8,18 @@ import java.util.List;
 
 @Setter
 @Getter
+@AllArgsConstructor
 public class LoginResponse {
-    private String jwtToken;
-    private String username;    
+    //private String jwtToken;
+    
+    private String username;   
+    private String email; 
     private List<String> roles;
 
-    public LoginResponse(String username, List<String> roles, String jwtToken) {
-        this.username = username;        
-        this.roles = roles;
-        this.jwtToken = jwtToken;
-    }
+    // public LoginResponse(String username, List<String> roles, String jwtToken) {
+    //     this.username = username;        
+    //     this.roles = roles;
+    //     this.jwtToken = jwtToken;
+    // }
 
 }
