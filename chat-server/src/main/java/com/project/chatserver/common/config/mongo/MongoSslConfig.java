fd80540ab44b2f48ff6d96f4572668d012163bf0
ruke79 <ruke79@naver.com>
2025-08-27 23:36:53 +0900

    fix mongodb ssl setting

--- chat-server/src/main/java/com/project/chatserver/common/config/mongo/MongoSslConfig.java
+++ chat-server/src/main/java/com/project/chatserver/common/config/mongo/MongoSslConfig.java
@@ -55,6 +55,11 @@ public class MongoSslConfig {
     @Value("${spring.data.mongodb.keystorePassword}")
     private String keystorePassword; //your CA Certifcate decoded (starts with BEGIN CERTIFICATE)
     
+    // @Value("${spring.data.mongodb.truststore}") // 새로운 프로퍼티로 truststore 파일 경로 지정
+    // private String truststoreFile;
+    // @Value("${spring.data.mongodb.truststorePassword}") // truststore 비밀번호 지정
+    // private String truststorePassword;
+
 
     @Bean
     public MongoTemplate mongoTemplate() {
@@ -64,12 +69,20 @@ public class MongoSslConfig {
     @Bean
     public MongoClient mongoClient() {
         
-        try {           
+        try {   
+            
+            // TrustStore 설정: truststore 파일을 직접 로드
+            // KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            // FileInputStream trustStoreStream = new FileInputStream(truststoreFile);
+            // trustStore.load(trustStoreStream, truststorePassword.toCharArray());
+            // trustStoreStream.close();
              
+            // .pem CA 인증서 설정 (신뢰할 수 있는 인증서)
             FileInputStream caFileStream  = new FileInputStream(caCertificatePath);
             CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
             X509Certificate caCert = (X509Certificate) certificateFactory.generateCertificate(caFileStream );
             caFileStream.close();
+            
 
             KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
             trustStore.load(null);
@@ -88,7 +101,10 @@ public class MongoSslConfig {
             kmf.init(keyStore, keystorePassword.toCharArray());
             
             SSLContext sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(null, tmf.getTrustManagers(), null);
+            sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);  // 이 부분이 중요
+
+            // KeyManagerFactory와 TrustManagerFactory 모두 사용하여 상호 TLS 설정
+            //sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
 
             // MongoClientSettings에 SSL Context 설정
             MongoClientSettings settings = MongoClientSettings.builder()
