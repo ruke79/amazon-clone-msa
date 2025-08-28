2dca12745c3573f76efca3c4fa184b9e52591aa3
ruke79 <ruke79@naver.com>
2025-08-28 20:33:57 +0900

    add kafka ssl setting in pay service

--- coupon-service/src/main/java/com/project/coupon_service/config/KafkaConsumerConfig.java
+++ coupon-service/src/main/java/com/project/coupon_service/config/KafkaConsumerConfig.java
@@ -1,12 +1,13 @@
 package com.project.coupon_service.config;
 
-
 import com.google.common.collect.ImmutableMap;
 import com.project.common.message.dto.request.CouponRollbackRequest;
 import com.project.common.message.dto.request.UserCreatedRequest;
 import com.project.common.message.dto.response.PaymentResponse;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.config.SslConfigs;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -19,12 +20,35 @@ import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
+
 @EnableKafka
 @Configuration
 public class KafkaConsumerConfig {
+    @Value("${kafka.security.protocol}")
+    private String securityProtocol;
+
+    @Value("${kafka.ssl.trust-store-password}")
+    private String trustStorePassword;
+
+    @Value("${kafka.ssl.trust-store-location}")
+    private String trustStoreLocation;
+
+    @Value("${kafka.ssl.key-store-location}")
+    private String keyStoreLocation;
+
+    @Value("${kafka.ssl.key-store-password}")
+    private String keyStorePassword;
+
     @Value("${kafka.url}")
     private String kafkaServerUrl;
-   
+
+    String checkPath(String keystorePath) {
+        if (keystorePath.startsWith("file://"))
+            return keystorePath.replaceFirst("file://", "");
+        else if (keystorePath.startsWith("file:"))
+            return keystorePath.replaceFirst("file:", "");
+        return keystorePath;
+    }
 
     @Bean
     ConcurrentKafkaListenerContainerFactory<String, UserCreatedRequest> kafkaListenerContainerFactory() {
@@ -33,45 +57,75 @@ public class KafkaConsumerConfig {
         factory.setBatchListener(false);
         return factory;
     }
+
     @Bean
     public ConsumerFactory<String, UserCreatedRequest> userCreatedConsumerFactory() {
         JsonDeserializer<UserCreatedRequest> deserializer = new JsonDeserializer<>();
         deserializer.addTrustedPackages("*");
-        
-        Map<String, Object> consumerConfigurations =
-                ImmutableMap.<String, Object>builder()
-                        .put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServerUrl)
-                        .put(ConsumerConfig.GROUP_ID_CONFIG, "user-2")
-                        .put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class)
-                        .put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer)
-                        .put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
-                        .build();
+
+        Map<String, Object> consumerConfigurations = ImmutableMap.<String, Object>builder()
+                .put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServerUrl)
+                .put(ConsumerConfig.GROUP_ID_CONFIG, "user-2")
+                .put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class)
+                .put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer)
+                .put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed")
+                .put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
+                .put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false)
+                .put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG,
+                        checkPath(keyStoreLocation))
+                .put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG,
+                        keyStorePassword)
+                .put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG,
+                        "JKS")
+                .put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG,
+                        checkPath(trustStoreLocation))
+                .put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG,
+                        trustStorePassword)
+                .put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG,
+                        "JKS")
+                .put(AdminClientConfig.SECURITY_PROTOCOL_CONFIG,
+                        securityProtocol)
+                .build();
 
         return new DefaultKafkaConsumerFactory<>(consumerConfigurations, new StringDeserializer(), deserializer);
     }
 
-     @Bean
+    @Bean
     ConcurrentKafkaListenerContainerFactory<String, CouponRollbackRequest> couponRollbackContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, CouponRollbackRequest> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(couponRollbackConsumerFactory());
         factory.setBatchListener(true);
         return factory;
     }
+
     @Bean
     public ConsumerFactory<String, CouponRollbackRequest> couponRollbackConsumerFactory() {
         JsonDeserializer<CouponRollbackRequest> deserializer = new JsonDeserializer<>();
         deserializer.addTrustedPackages("*");
-        
-        Map<String, Object> consumerConfigurations =
-                ImmutableMap.<String, Object>builder()
-                        .put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServerUrl)
-                        .put(ConsumerConfig.GROUP_ID_CONFIG, "coupon")
-                        .put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class)
-                        .put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer)
-                        .put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
-                        .put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false)
-                        .put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed") 
-                        .build();
+
+        Map<String, Object> consumerConfigurations = ImmutableMap.<String, Object>builder()
+                .put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServerUrl)
+                .put(ConsumerConfig.GROUP_ID_CONFIG, "coupon")
+                .put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class)
+                .put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer)
+                .put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed")
+                .put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
+                .put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false)
+                .put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG,
+                        checkPath(keyStoreLocation))
+                .put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG,
+                        keyStorePassword)
+                .put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG,
+                        "JKS")
+                .put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG,
+                        checkPath(trustStoreLocation))
+                .put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG,
+                        trustStorePassword)
+                .put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG,
+                        "JKS")
+                .put(AdminClientConfig.SECURITY_PROTOCOL_CONFIG,
+                        securityProtocol)
+                .build();
 
         return new DefaultKafkaConsumerFactory<>(consumerConfigurations, new StringDeserializer(), deserializer);
     }
@@ -83,21 +137,35 @@ public class KafkaConsumerConfig {
         factory.setBatchListener(true);
         return factory;
     }
+
     @Bean
     public ConsumerFactory<String, PaymentResponse> paymentResponseConsumerFactory() {
         JsonDeserializer<PaymentResponse> deserializer = new JsonDeserializer<>();
         deserializer.addTrustedPackages("*");
-        
-        Map<String, Object> consumerConfigurations =
-                ImmutableMap.<String, Object>builder()
-                        .put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServerUrl)
-                        .put(ConsumerConfig.GROUP_ID_CONFIG, "coupon-used")
-                        .put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class)
-                        .put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer)
-                        .put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
-                        .put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false)
-                        .put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed") 
-                        .build();
+
+        Map<String, Object> consumerConfigurations = ImmutableMap.<String, Object>builder()
+                .put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServerUrl)
+                .put(ConsumerConfig.GROUP_ID_CONFIG, "coupon-used")
+                .put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class)
+                .put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer)
+                .put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed")
+                .put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
+                .put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false)
+                .put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG,
+                        checkPath(keyStoreLocation))
+                .put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG,
+                        keyStorePassword)
+                .put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG,
+                        "JKS")
+                .put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG,
+                        checkPath(trustStoreLocation))
+                .put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG,
+                        trustStorePassword)
+                .put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG,
+                        "JKS")
+                .put(AdminClientConfig.SECURITY_PROTOCOL_CONFIG,
+                        securityProtocol)
+                .build();
 
         return new DefaultKafkaConsumerFactory<>(consumerConfigurations, new StringDeserializer(), deserializer);
     }
--- coupon-service/src/main/java/com/project/coupon_service/config/KafkaProducerConfig.java
+++ coupon-service/src/main/java/com/project/coupon_service/config/KafkaProducerConfig.java
@@ -1,6 +1,9 @@
 package com.project.coupon_service.config;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.config.SslConfigs;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -17,25 +20,65 @@ import com.project.common.message.dto.request.CouponUseRequest;
 
 import java.util.Map;
 import java.util.UUID;
+
 @EnableKafka
 @Configuration
 public class KafkaProducerConfig {
 
+    @Value("${kafka.security.protocol}")
+    private String securityProtocol;
+
+    @Value("${kafka.ssl.trust-store-password}")
+    private String trustStorePassword;
+
+    @Value("${kafka.ssl.trust-store-location}")
+    private String trustStoreLocation;
+
+    @Value("${kafka.ssl.key-store-location}")
+    private String keyStoreLocation;
+
+    @Value("${kafka.ssl.key-store-password}")
+    private String keyStorePassword;
+
     @Value("${kafka.url}")
     private String kafkaServerUrl;
+
+    String checkPath(String keystorePath) {
+        if (keystorePath.startsWith("file://"))
+            return keystorePath.replaceFirst("file://", "");
+        else if (keystorePath.startsWith("file:"))
+            return keystorePath.replaceFirst("file:", "");
+        return keystorePath;
+    }
 
     @Bean
     public Map<String, Object> producerConfigurations() {
         return ImmutableMap.<String, Object>builder()
                 .put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServerUrl)
                 .put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class)
-                .put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class)                
+                .put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class)
                 .put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true)
                 .put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, UUID.randomUUID().toString())
+                .put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed")
+                .put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
+                .put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false)
+                .put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG,
+                        checkPath(keyStoreLocation))
+                .put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG,
+                        keyStorePassword)
+                .put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG,
+                        "JKS")
+                .put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG,
+                        checkPath(trustStoreLocation))
+                .put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG,
+                        trustStorePassword)
+                .put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG,
+                        "JKS")
+                .put(AdminClientConfig.SECURITY_PROTOCOL_CONFIG,
+                        securityProtocol)
                 .build();
 
     }
-
 
     @Bean
     public ProducerFactory<String, CouponUseRequest> couponUseProducerFactory() {
@@ -47,5 +90,5 @@ public class KafkaProducerConfig {
     public KafkaTemplate<String, CouponUseRequest> couponUseKafkaTemplate() {
         return new KafkaTemplate<>(couponUseProducerFactory());
     }
-    
+
 }
