83627876351c848addb47d3da6d70deaa085ef4f
ruke79 <ruke79@naver.com>
2025-09-04 18:43:06 +0900

    fix bug

--- user-service/src/main/java/com/project/userservice/controller/NotificationController.java
+++ user-service/src/main/java/com/project/userservice/controller/NotificationController.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -20,6 +21,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
+@RequestMapping("/api/")
 @RequiredArgsConstructor
 public class NotificationController {
 
